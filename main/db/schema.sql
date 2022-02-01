e7186140f0f8de4141effd8b4651d0e312caa4b6
Michael Klein <Michaelklein1997@gmail.com>
2022-01-31 18:13:45 -0600

    added foreign and primary keys to tables

--- main/db/schema.sql
+++ main/db/schema.sql
@@ -3,22 +3,31 @@ CREATE DATABASE company_db;
 
 USE company_db;
 
-CREATE TABLE department (
+CREATE TABLE departments (
     department_id INT NOT NULL,
     department_name VARCHAR(25) NOT NULL,
+    PRIMARY KEY (department_id)
 );
 
-CREATE TABLE role (
-    id INT NOT NULL,
+CREATE TABLE roles (
+    role_id INT NOT NULL,
     role_title VARCHAR(25) NOT NULL,
     salary INT NOT NULL,
-    department_id INT NOT NULL,
+    department_id INT NOT NULL, --FK
+    PRIMARY KEY (role_id)
+    FOREIGN KEY (department_id)
+    REFERENCES (departments.department_id)
+    ON DELETE SET NULL
 );
 
-CREATE TABLE role (
-    id INT NOT NULL,
+CREATE TABLE employees (1
+    employee_id INT NOT NULL, --PK1
     first_name VARCHAR(15) NOT NULL,
     last_name VARCHAR(15) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT NOT NULL
-)
+    role_id INT NOT NULL, --FK
+    manager_id INT NOT NULL, --FK1
+    PRIMARY KEY (employee_id)
+    FOREIGN KEY (role_id)
+    REFERENCES (roles.roles_id)
+    ON DELETE SET NULL
+);
