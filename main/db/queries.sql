41917f954575162b10bdde42eb6aaeaf2c8c0298
Michael Klein <Michaelklein1997@gmail.com>
2022-01-31 20:43:30 -0600

    added all necessary queries to query.sql

--- main/db/queries.sql
+++ main/db/queries.sql
@@ -21,3 +21,24 @@ SELECT *
 FROM departments
 JOIN roles ON departments.department_id = roles.department_id
 JOIN employees ON roles.role_id = employees.role_id;
+
+-- Add A Department
+INSERT INTO departments (department_id, department_name)
+    VALUES 
+        (?,?);
+
+-- Add A Role
+INSERT INTO roles (role_id, role_title, salary, department_name)
+    VALUES 
+        (?,?,?);
+
+-- Add A Employee
+INSERT INTO roles (employee_id, first_name, last_name, role_title, manager_id)
+    VALUES 
+        (?,?,?,?);
+
+-- Update An Employee Role
+
+UPDATE employees
+SET role_id = ?
+WHERE employee_id = ?
