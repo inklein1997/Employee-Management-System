2a939e5da4d72b22d1be5a817a959a26956f9bab
Michael Klein <Michaelklein1997@gmail.com>
2022-02-03 21:21:52 -0600

    employee table now displays manager names

--- index.js
+++ index.js
@@ -33,10 +33,12 @@ const displayRoles = () => {
 
 // NEED TO FIX MANAGERS COLUMN!
 const displayEmployees = () => {
-    const sqlQuery = `SELECT employee_id AS id, first_name, last_name, role_title, department_name, salary, manager_id
-    FROM departments
-    JOIN roles ON departments.department_id = roles.department_id
-    JOIN employees ON roles.role_id = employees.role_id;`
+    const sqlQuery = `SELECT emp.employee_id AS id, emp.first_name, emp.last_name, role_title, department_name, emp.manager_id, man.first_name AS manager_first_name, man.last_name AS manager_last_name
+    FROM employees emp
+    JOIN roles ON emp.role_id = roles.role_id
+    LEFT JOIN departments ON roles.department_id= departments.department_id
+    LEFT JOIN employees man
+    ON man.employee_id = emp.manager_id;`
     db.query(sqlQuery, (err, response) => {
         if (err) {
             console.log(err);
